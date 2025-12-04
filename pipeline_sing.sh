71c7a48edfc00ea33651ab234fe39c6612cc811c
emilioispano <emilio.ispano@phd.unipd.it>
2025-12-04 13:51:28 +0100

    bug fix

--- pipeline_sing.sh
+++ pipeline_sing.sh
@@ -51,7 +51,7 @@ done
 # -----------------------------
 # Check required arguments
 # -----------------------------
-if [[ -z "$diamond_db" || -z "$source_fasta" || -z "$results" ) ]]; then
+if [[ -z "$diamond_db" || -z "$source_fasta" || -z "$results"  ]]; then
     echo "Error: -d <db>, -f <fasta>, and -o <outdir> are required arguments"
     exit 1
 fi
@@ -59,7 +59,7 @@ fi
 # -----------------------------
 # Run pipeline
 # -----------------------------
-$src=/argot25/src
+src=/argot25/src
 
 
 echo "=== CHECKING FASTA HEADER FORMAT ==="
@@ -68,31 +68,34 @@ mv "${source_fasta}.clean" "$source_fasta"
 
 echo "=== RUNNING DIAMOND & ARGOT PIPELINE ==="
 mkdir -p $results $results/data/input $results/data/output $results/predictions
+input=$results/data/input
+output=$results/data/output
+preds=$results/predictions
 
 echo "Running DIAMOND..."
-$diamond blastp -d $diamond_db -q $source_fasta -o $results/output/diamond_raw.blastp -f 6 -b 5 -c 1 -k 1000 -p "$threads"
+$diamond blastp -d $diamond_db -q $source_fasta -o $output/diamond_raw.blastp -f 6 -b 5 -c 1 -k 1000 -p $threads
 
 echo "Cleaning DIAMOND output..."
-if [ -f $results/output/diamond_clean.blastp ]
+if [ -f $output/diamond_clean.blastp ]
 then
-    rm $results/output/diamond_clean.blastp
+    rm $output/diamond_clean.blastp
 fi
-python3 $src/clean_blastp.py -i $results/output/diamond_raw.blastp -o $results/output/diamond_clean.blastp
+python3 $src/clean_blastp.py -i $output/diamond_raw.blastp -o $output/diamond_clean.blastp
 
 echo "Creating Argot input..."
-python3 $src/new_blastp_to_argot_inp.py -b $resulrs/output/diamond_clean.blastp -m localhost -d ARGOT_NEW -c annots -o $results/input/argot_in.txt
+python3 $src/new_blastp_to_argot_inp.py -b $output/diamond_clean.blastp -m localhost -d ARGOT_NEW -c annots -o $input/argot_in.txt
 
 echo "Running Argot..."
-$argot -i $results/input/argot_in.txt -s localhost -d ARGOT_NEW -o $results/output/argot_out.txt -g /argot25/support_files/go.owl -c goafreq
+$argot -i $input/argot_in.txt -s localhost -d ARGOT_NEW -o $output/argot_out.txt -g /argot25/support_files/go.owl -c goafreq
 
 echo "Converting to CAFA format..."
-python3 $src/in-cafa_format.py -i $results/output/argot_out.txt -v Argot3 -o $results/output/ -f temporary
+python3 $src/in-cafa_format.py -i $output/argot_out.txt -v Argot3 -o $output/ -f temporary
 
 echo "Propagating GO terms..."
-mv $results/output/temporary_argot_out_in_cafa.txt $results/output/predictions_raw.tsv"
-python3 $src/propagate.py -i $results/output/predictions_raw.tsv -o $results/output/predictions_prop.tsv -g /argot25/support_files/go.owl -p
+mv $output/temporary_argot_out_in_cafa.txt $output/predictions_raw.tsv"
+python3 $src/propagate.py -i $output/predictions_raw.tsv -o $output/predictions_prop.tsv -g /argot25/support_files/go.owl -p
 
-python3 $src/format_out.py -i $results/output/predictions_raw.tsv -o $results/predictions/unpropagated.tsv -g /argot25/support_files/go.owl
-python3 $src/format_out.py -i $results/output/predictions_prop.tsv -o $results/predictions/propagated.tsv -g /argot25/support_files/go.owl
+python3 $src/format_out.py -i $output/predictions_raw.tsv -o $preds/unpropagated.tsv -g /argot25/support_files/go.owl
+python3 $src/format_out.py -i $output/predictions_prop.tsv -o $preds/propagated.tsv -g /argot25/support_files/go.owl
 
 echo "=== DONE ==="
